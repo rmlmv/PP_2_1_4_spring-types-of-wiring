fc556380f81479c7eee5a35c52427b33797dee55
Roman Lomov <romario.lomov@gmail.com>
2024-07-20 17:57:25 +0600

    Add DI

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,11 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+    @Override
+    public String toString() {
+        return "Если смерть на игле, значит не такой уж и бессмертный ;)";
+    }
 }
